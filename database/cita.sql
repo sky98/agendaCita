5d2eae8090e26ee5b83c805a0424ab719afd381d
Lorenzo Rojo <lorenzorojo12@gmail.com>
2019-06-12 23:59:12 -0500

    Archivo de Base de datos

---
+++ database/cita.sql
@@ -0,0 +1,434 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-06-2019 a las 06:57:53
+-- Versión del servidor: 10.3.15-MariaDB
+-- Versión de PHP: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `reservacion` datetime NOT NULL,
+  `notas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Reservada','En Proceso','FInalizada','Cancelada') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Reservada',
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `sede_id` int(10) UNSIGNED NOT NULL,
+  `servicio_id` int(10) UNSIGNED NOT NULL,
+  `profesional_id` int(10) UNSIGNED NOT NULL,
+  `cliente_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cedula` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sede_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `name`, `cedula`, `email`, `telefono`, `address`, `sede_id`, `created_at`, `updated_at`) VALUES
+(1, 'Pedro Jose Infante', '123456789', 'pedroinfate@gmail.com', '3023356659', 'cl 20f cr 76-144', 2, '2019-06-13 04:57:07', '2019-06-13 05:07:18');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_01_08_144959_create_sedes_table', 1),
+(4, '2019_01_08_144960_create_clientes_table', 1),
+(5, '2019_01_08_144961_create_profesionales_table', 1),
+(6, '2019_01_08_144962_create_servicios_table', 1),
+(7, '2019_01_08_151322_create_citas_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesionales`
+--
+
+CREATE TABLE `profesionales` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cedula` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('activo','suspendido') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'activo',
+  `sede_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `profesionales`
+--
+
+INSERT INTO `profesionales` (`id`, `name`, `cedula`, `email`, `telefono`, `status`, `sede_id`, `created_at`, `updated_at`) VALUES
+(1, 'profesional 1', '12345678', 'profesional1@gmail.com', '320345667', 'activo', 2, '2019-06-13 04:33:44', '2019-06-13 04:33:44'),
+(2, 'profesional2', '987654312', 'profesional2@gmail.com', '3001234567', 'activo', 1, '2019-06-13 04:34:29', '2019-06-13 04:34:29');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesional_servicio`
+--
+
+CREATE TABLE `profesional_servicio` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `profesional_id` int(10) UNSIGNED NOT NULL,
+  `servicio_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `profesional_servicio`
+--
+
+INSERT INTO `profesional_servicio` (`id`, `profesional_id`, `servicio_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, NULL, NULL),
+(2, 1, 3, NULL, NULL),
+(3, 2, 1, NULL, NULL),
+(4, 2, 2, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sedes`
+--
+
+CREATE TABLE `sedes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('activo','suspendido') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'activo',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sedes`
+--
+
+INSERT INTO `sedes` (`id`, `name`, `address`, `telefono`, `email`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Sede Medellín', 'Cl 20 #65a-12', '3005555555', 'sede1@gmail.com', 'activo', '2019-06-13 04:25:37', '2019-06-13 04:25:37'),
+(2, 'Sede Bello', 'Cl 20f ·76-144', '3023356659', 'sede2@gmail.com', 'activo', '2019-06-13 04:26:07', '2019-06-13 04:26:07');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sede_servicio`
+--
+
+CREATE TABLE `sede_servicio` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `sede_id` int(10) UNSIGNED NOT NULL,
+  `servicio_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sede_servicio`
+--
+
+INSERT INTO `sede_servicio` (`id`, `sede_id`, `servicio_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2019-06-13 04:31:13', '2019-06-13 04:31:13'),
+(2, 2, 1, '2019-06-13 04:31:13', '2019-06-13 04:31:13'),
+(3, 1, 2, '2019-06-13 04:31:24', '2019-06-13 04:31:24'),
+(4, 2, 3, '2019-06-13 04:31:37', '2019-06-13 04:31:37');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `costo` decimal(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id`, `name`, `costo`, `created_at`, `updated_at`) VALUES
+(1, 'Servicio 1', '100000.00', '2019-06-13 04:31:13', '2019-06-13 04:31:13'),
+(2, 'Servicio 2', '50000.00', '2019-06-13 04:31:24', '2019-06-13 04:31:24'),
+(3, 'Servicio3', '40000.00', '2019-06-13 04:31:37', '2019-06-13 04:31:37');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('estandar','administrador') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'estandar',
+  `status` enum('activo','suspendido') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'activo',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `type`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Administrador', 'admin@panel.com', NULL, '$2y$10$vpAC0AA9zd8Ct1nLKk38jOAYfKOw2ou1z.cxQG91g..oVQ83aWVqm', 'administrador', 'activo', NULL, '2019-06-13 04:23:22', '2019-06-13 04:23:22'),
+(2, 'invitado', 'invtado@panel.com', NULL, '$2y$10$XOTXoRi3bolguVAxpCr1D.MHuUakLKFWsk3NQJ53VtAvB0CtDZstq', 'estandar', 'activo', NULL, '2019-06-13 04:29:01', '2019-06-13 04:29:01');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `citas_user_id_foreign` (`user_id`),
+  ADD KEY `citas_sede_id_foreign` (`sede_id`),
+  ADD KEY `citas_servicio_id_foreign` (`servicio_id`),
+  ADD KEY `citas_profesional_id_foreign` (`profesional_id`),
+  ADD KEY `citas_cliente_id_foreign` (`cliente_id`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `clientes_sede_id_foreign` (`sede_id`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `profesionales`
+--
+ALTER TABLE `profesionales`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profesionales_sede_id_foreign` (`sede_id`);
+
+--
+-- Indices de la tabla `profesional_servicio`
+--
+ALTER TABLE `profesional_servicio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profesional_servicio_profesional_id_foreign` (`profesional_id`),
+  ADD KEY `profesional_servicio_servicio_id_foreign` (`servicio_id`);
+
+--
+-- Indices de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sedes_email_unique` (`email`);
+
+--
+-- Indices de la tabla `sede_servicio`
+--
+ALTER TABLE `sede_servicio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sede_servicio_sede_id_foreign` (`sede_id`),
+  ADD KEY `sede_servicio_servicio_id_foreign` (`servicio_id`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `profesionales`
+--
+ALTER TABLE `profesionales`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `profesional_servicio`
+--
+ALTER TABLE `profesional_servicio`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `sede_servicio`
+--
+ALTER TABLE `sede_servicio`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `citas_cliente_id_foreign` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `citas_profesional_id_foreign` FOREIGN KEY (`profesional_id`) REFERENCES `profesionales` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `citas_sede_id_foreign` FOREIGN KEY (`sede_id`) REFERENCES `sedes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `citas_servicio_id_foreign` FOREIGN KEY (`servicio_id`) REFERENCES `servicios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `citas_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `clientes_sede_id_foreign` FOREIGN KEY (`sede_id`) REFERENCES `sedes` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `profesionales`
+--
+ALTER TABLE `profesionales`
+  ADD CONSTRAINT `profesionales_sede_id_foreign` FOREIGN KEY (`sede_id`) REFERENCES `sedes` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `profesional_servicio`
+--
+ALTER TABLE `profesional_servicio`
+  ADD CONSTRAINT `profesional_servicio_profesional_id_foreign` FOREIGN KEY (`profesional_id`) REFERENCES `profesionales` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `profesional_servicio_servicio_id_foreign` FOREIGN KEY (`servicio_id`) REFERENCES `servicios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `sede_servicio`
+--
+ALTER TABLE `sede_servicio`
+  ADD CONSTRAINT `sede_servicio_sede_id_foreign` FOREIGN KEY (`sede_id`) REFERENCES `sedes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `sede_servicio_servicio_id_foreign` FOREIGN KEY (`servicio_id`) REFERENCES `servicios` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
